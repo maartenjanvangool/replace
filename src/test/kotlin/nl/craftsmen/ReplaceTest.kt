379edc578fe47bf4fcc7d1463b6a9bf09a51aa1c
maarten-jan <maarten-jan@craftsmen.nl>
2023-11-25 16:45:57 +0100

    extra test

--- src/test/kotlin/nl/craftsmen/ReplaceTest.kt
+++ src/test/kotlin/nl/craftsmen/ReplaceTest.kt
@@ -3,12 +3,15 @@ package nl.craftsmen
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
+import io.micronaut.core.io.ResourceResolver
+import io.micronaut.core.io.scan.ClassPathResourceLoader
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
+
 
 class ReplaceTest {
 
@@ -34,5 +37,16 @@ class ReplaceTest {
         PicocliRunner.run(Replace::class.java, ctx, *args)
 
         baos.toString().trimIndent() `should be equal to` "vlavlavla"
+    }
+
+    @Test
+    fun `input file text is replaced`() {
+        val loader: ClassPathResourceLoader = ResourceResolver().getLoader(ClassPathResourceLoader::class.java).get()
+        val resource = loader.getResource("classpath:bla.txt").get()
+
+        val args = arrayOf("-f", resource.file, "bla", "vla")
+        PicocliRunner.run(Replace::class.java, ctx, *args)
+
+        baos.toString().trimIndent().trimIndent() `should be equal to` "vlavlavla"
     }
 }
